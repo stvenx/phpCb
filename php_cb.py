82d52348efb541499501c63729f4a75c767effcf
Seven Heart <stvenx@gmail.com>
2012-09-12 10:33:48 +0800

    fix run error

--- php_cb.py
+++ php_cb.py
@@ -1,21 +1,24 @@
-import sublime, sublime_plugin, re, subprocess,os
+import sublime, sublime_plugin, re, subprocess,os,sys
 from subprocess import Popen
 from subprocess import PIPE
-
+built_in_plugins = os.path.join(sublime.packages_path(), 'PhpCb')
+if not built_in_plugins in sys.path:
+    sys.path.append(built_in_plugins)
 class PhpCbCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         FILE = self.view.file_name()
+        path=sublime.packages_path()+'\\PhpCb'
         if FILE[-3:] == 'php':
             allFile = sublime.Region(0, self.view.size())
             AllFileText = self.view.substr(allFile).encode('utf-8')
-            tmpFile=os.getcwd()+"\\temp.php"
+            tmpFile=path+"\\temp.php"
             tfp=open(tmpFile,"w");
             tfp.writelines(AllFileText)
             tfp.close()
-            cmd=".\\phpcb.exe"+" --space-after-if --optimize-eol --space-after-switch --space-after-while --space-before-start-angle-bracket --space-after-end-angle-bracket --extra-padding-for-case-statement  --align-equal-statements --force-large-php-code-tag --glue-amperscore --change-shell-comment-to-double-slashes-comment --indent-with-tab --force-large-php-code-tag --force-true-false-null-contant-lowercase --comment-rendering-style PEAR --padding-char-count 1 "+'"'+tmpFile+'"'
+            cmd="phpcb.exe"+" --space-after-if --optimize-eol --space-after-switch --space-after-while --space-before-start-angle-bracket --space-after-end-angle-bracket --extra-padding-for-case-statement  --align-equal-statements --force-large-php-code-tag --glue-amperscore --change-shell-comment-to-double-slashes-comment --indent-with-tab --force-large-php-code-tag --force-true-false-null-contant-lowercase --comment-rendering-style PEAR --padding-char-count 1 "+'"'+tmpFile+'"'
             p=Popen(cmd,stdout=PIPE,stderr=PIPE,shell=True)
             stdout, stderr = p.communicate(AllFileText)
-            os.remove(tmpFile)
+            #os.remove(tmpFile)
             if len(stderr) == 0:
                 self.view.replace(edit, allFile, self.fixup(stdout))
             else:
